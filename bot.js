a4f84076730985b24bd106e066ba9e845673b94c
FenTaylor <malik.sergey@gmail.com>
2023-10-30 01:02:44 -0500

    .

--- bot.js
+++ bot.js
@@ -10,13 +10,6 @@ const bot = new TelegramBot(token, { polling: true });
 bot.on("polling_error", console.log);
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
-  const allowedChats = [-1001878806793, -1002112816469, -4051808094];
-
-  if (!allowedChats.includes(chatId) && msg.chat.type != 'private') {
-    console.log(`Access restricted for: ${chatId}`, msg);
-    return false
-  };
-
   const botReply = handler(msg);
 
   if (botReply) {
--- handlers/index.js
+++ handlers/index.js
@@ -1,9 +1,15 @@
 const greeting = require('./greeting');
+const allowedChats = [-1001878806793, -1002112816469];
 
-const buildReply = function(msg) {
+const buildReply = function (msg) {
     if (msg.new_chat_members || msg.text === '/start') {
         const newMemberName = msg.new_chat_member?.first_name ?? msg.from?.first_name ?? 'Синебот';
-        
+
+        if (!allowedChats.includes(msg.chat.id) && msg.chat.type != 'private') {
+            console.log(`Access restricted for: ${msg.chat.id}`, msg);
+            return false
+        };
+
         return {
             response: greeting(newMemberName),
             originMsgId: msg.message_id
