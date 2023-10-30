e936db024ad7e868fc9e04afef2ea975e778bfb4
FenTaylor <malik.sergey@gmail.com>
2023-10-29 20:50:08 -0500

    ...

--- handlers/index.js
+++ handlers/index.js
@@ -1,14 +1,12 @@
 const greeting = require('./greeting');
 
-function buildReply(msg) {
-    console.log(msg);
-
+const buildReply = function(msg) {
     if (msg.new_chat_members || msg.text === '/start') {
         const newMemberName = msg.from?.first_name ?? 'Синебот';
         
         return greeting(newMemberName);
     }
-
+    
     return false;
 }
 
